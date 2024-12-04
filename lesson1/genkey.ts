b363c394131fe55335c7138b7d4c28bf74a148d6
Jan Genoch <j-scrap>
2024-12-04 04:22:08 -0500

    Command Line Options for Lesson 1
    Lesson 1's genkey.ts will now read a key from the .env file, or generate a new key, depending on the options supplied in the command line.

--- lesson1/genkey.ts
+++ lesson1/genkey.ts
@@ -1,7 +1,35 @@
+import { parse } from "ts-command-line-args";
 import "dotenv/config";
+import { Keypair } from "@solana/web3.js";
 import { getKeypairFromEnvironment } from "@solana-developers/helpers";
 
-const keypair = getKeypairFromEnvironment("SECRET_KEY");
+interface IGenKeyArgs {
+	genKey: boolean;
+	readKey?: string;
+	help: boolean;
+}
 
-console.log(`The public key is: `, keypair.publicKey.toBase58());
-console.log(`The private key is: `, keypair.secretKey);
+const args = parse<IGenKeyArgs>(
+	{
+		genKey: { type: Boolean, alias: 'g', description: "Prints info for a newly generated Key" },
+		readKey: { type: String, optional: true, alias: 'r', description: "Shows info for the specified Key" },
+		help: { type: Boolean, alias: 'h', description: "Prints the Help" }
+	},
+	{
+		helpArg: "help"
+	}
+);
+
+if (args.readKey != null && args.readKey) {
+	const keypair = getKeypairFromEnvironment(`SECRET_KEY_${args.readKey}`);
+
+	console.log(`The public key is: `, keypair.publicKey.toBase58());
+	console.log(`The private key is: `, keypair.secretKey);
+}
+
+if (args.genKey) {
+	 const keypair = Keypair.generate();
+
+	 console.log(`The public key is: `, keypair.publicKey.toBase58());
+	 console.log(`The secret key is: `, keypair.secretKey);
+}
